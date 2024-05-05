a2110173fb1a2379da29ec250fbdc6d3965874a0
NemesisVR <168294740+NemesisVR@users.noreply.github.com>
2024-05-05 18:47:01 +0000

    hola.py

--- Modulo1/hola.py
+++ Modulo1/hola.py
@@ -1,7 +1,6 @@
-#ejecutando hola mundo
+# ejecutando hola mundo
 
-#clear -> sobre terminar para limpiar mi terminal (en windows el comando se llama "cls")
-#cd -> cambiar directorio sobre terminal
-# python <nombre_archivo.py ->para ejecutar mi programa python sobre la terminal
-
-print('hola mundo')
+# clear -> sobre terminal para limpiar mi terminal (en windows el comando se llama "cls")
+# cd -> cambiar directorio sobre terminal
+# python <nombre_archivo>.py -> para ejecutar mi programa python sobre la terminal
+print('Hola mundo')
